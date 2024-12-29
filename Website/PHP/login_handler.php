3299c8e0853056b15c980b60dd1b79b928f6b79f
Martin Tomassi <martin.tomassi@studio.unibo.it>
2024-12-29 21:48:41 +0100

    Added Password Check and Modified Mail Check if existing, in case of Login.

--- Website/PHP/login_handler.php
+++ Website/PHP/login_handler.php
@@ -1,9 +1,25 @@
 <?php
 require_once("bootstrap.php");
 
-if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["emailinsert"])) {
+if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["emailinsert"]) && !empty($_POST["emailinsert"])) {
     header('Content-Type: application/json');
-    $response = ["exists" => $dbh->isUserRegistered($_POST["emailinsert"])];
+    $email = $_POST["emailinsert"];
+    if (isset($_POST["password"])) {
+        $password = $_POST["password"];
+        $user = $dbh->loginUser($email, $password);
+    } else {
+        $response = ["exists" => $dbh->isUserRegistered($_POST["emailinsert"])];
+        echo json_encode($response);
+        exit();
+    }
+
+    if ($user) {
+        $response = ["success" => true, "message" => "Login successful"];
+    } else {
+        $response = ["success" => false, "message" => "Invalid email or password"];
+    }
+
     echo json_encode($response);
+    exit();
 }
 ?>
