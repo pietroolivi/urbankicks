f8d9284b63fa8f4384ce95bea972e0832655def9
Martin Tomassi <martin.tomassi@studio.unibo.it>
2025-01-08 14:32:00 +0100

    Modified Index from HTML to PHP.

---
+++ Website/Template/index_content.php
@@ -0,0 +1,30 @@
+<div class="explore-section">
+    <img src="CSS/Images/Illustrations/skater.svg" alt="Skater illustration">
+    <a href="#" class="explore-now">Explore Now</a>
+</div>
+
+<nav class="brands">
+    <h2>Our Brands</h2>
+    <ul>
+        <li><a href=""><img src="CSS/Images/Icons/adidas.svg" alt="Adidas products."></a></li>
+        <li><a href=""><img src="CSS/Images/Icons/new_balance.svg" alt="New Balance products."></a></li>
+        <li><a href=""><img src="CSS/Images/Icons/sergio_tacchini.svg" alt="Sergio Tacchini products."></a></li>
+        <li><a href=""><img src="CSS/Images/Icons/crocs.svg" alt="Crocs products."></a></li>
+        <li><a href=""><img src="CSS/Images/Icons/fila.svg" alt="Fila products."></a></li>
+        <li><a href=""><img src="CSS/Images/Icons/converse.svg" alt="Converse products."></a></li>
+        <li><a href=""><img src="CSS/Images/Icons/asics.svg" alt="Asics products."></a></li>
+        <li><a href=""><img src="CSS/Images/Icons/hoka.svg" alt="Hoka products."></a></li>
+        <li><a href=""><img src="CSS/Images/Icons/lacoste.svg" alt="Lacoste products."></a></li>
+        <li><a href=""><img src="CSS/Images/Icons/on.svg" alt="On products."></a></li>
+        <li><a href=""><img src="CSS/Images/Icons/puma.svg" alt="Puma products."></a></li>
+        <li><a href=""><img src="CSS/Images/Icons/reebok.svg" alt="Reebok products."></a></li>
+        <li><a href=""><img src="CSS/Images/Icons/salomon.svg" alt="Salomon products."></a></li>
+        <li><a href=""><img src="CSS/Images/Icons/ugg.svg" alt="Ugg products."></a></li>
+        <li><a href=""><img src="CSS/Images/Icons/saucony.svg" alt="Saucony products."></a></li>
+        <li><a href=""><img src="CSS/Images/Icons/vans.svg" alt="Vans products."></a></li>
+        <li><a href=""><img src="CSS/Images/Icons/havaianas.svg" alt="Havaianas products."></a></li>
+        <li><a href=""><img src="CSS/Images/Icons/kappa.svg" alt="Kappa products."></a></li>
+        <li><a href=""><img src="CSS/Images/Icons/under_armour.svg" alt="Under Armour products."></a></li>
+        <li><a href=""><img src="CSS/Images/Icons/diadora.svg" alt="Diadora products."></a></li>
+    </ul>
+</nav>
